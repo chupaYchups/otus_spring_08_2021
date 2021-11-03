0d7bb3ed71718da11b1b8bf31cf98661a531e25d
Ivan Chupakhin <i.chupakhin@ase-ec.ru>
2021-11-03 23:23:26 +0300

    add manager service with print method

--- hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/service/QuizManagerService.java
+++ hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/service/QuizManagerService.java
@@ -1,6 +1,8 @@
 package ru.chupaYchups.service;
 
+import java.io.IOException;
+
 public interface QuizManagerService {
     void doAQuiz();
-    void printQuestions();
+    void printQuestions() throws IOException;
 }
