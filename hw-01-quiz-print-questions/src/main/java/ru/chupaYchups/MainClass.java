25fecae8be1d8c0238f7393cc24072fe87cc4202
Ivan Chupakhin <i.chupakhin@ase-ec.ru>
2021-11-03 23:25:31 +0300

    add throw exception

--- hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/MainClass.java
+++ hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/MainClass.java
@@ -3,9 +3,11 @@ package ru.chupaYchups;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.chupaYchups.service.QuizManagerService;
 
+import java.io.IOException;
+
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
         QuizManagerService quizManagerService = context.getBean(QuizManagerService.class);
         quizManagerService.printQuestions();
